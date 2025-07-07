e7198d823d281e5bd10750e814060b79a546fc81
Estefany Cruz <estefannycruz2021@gmail.com>
2025-07-07 11:19:34 -0700

    SEO:Actualiza titulos y meta descripciones para Google Search Console

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+  // src/consts.ts
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+    // Título principal del sitio para SEO general y como fallback
+    export const SITE_TITLE = 'Servicios Electromecánicos SCJ | Embobinados, Compresores, Seguridad en Tijuana, camaras de seguridad (CCTV), alarmas';
+
+    // Descripción principal del sitio para SEO general y como fallback
+    export const SITE_DESCRIPTION = 'Expertos en servicios electromecánicos en Tijuana, Baja California. Ofrecemos embobinados de motores, mantenimiento de compresores y soluciones de seguridad electrónica. ¡Calidad y experiencia garantizadas!';
+    
